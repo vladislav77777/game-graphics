bd36de4d9bb7502abd4429a2de6c3e1346fb4bec
vladislav77777 <vladoss7700@mail.ru>
2024-10-02 10:28:07 +0300

    rasterization

--- README.md
+++ README.md
@@ -35,3 +35,20 @@ cmake ..
 - [Cube model](https://casual-effects.com/g3d/data10/index.html#) by Morgan McGuire (CC BY 3.0 License)
 - [Teapot model](https://casual-effects.com/g3d/data10/common/model/teapot/teapot.zip) by Martin Newell (CC0 License)
 - [Dabrovic Sponza model](https://casual-effects.com/g3d/data10/index.html#) by Marko Dabrovic (CC BY-NC License)
+
+
+# Creative Task Description for Rasterization
+### Files Modified and Their Purpose
+
+- **rasterizer_renderer.cpp**: The `apply_sepia_tone_filter` method called for the sepia tone filter effect.
+- **rasterizer.h**: Added the `apply_sepia_tone_filter` method, which applies a sepia filter to the rendered image by iterating over pixels and converting them using the sepia formula.
+
+The task implements rasterization, applying a sepia tone filter to the final render output, transforming pixel colors to create a classic visual effect.
+
+- Before:
+![image](https://github.com/user-attachments/assets/9f455c42-38c1-4c66-b52a-03344bc325bb)
+
+
+- After:
+![image](https://github.com/user-attachments/assets/0784ea17-e3e7-485d-bb88-463a2ddf440b)
+
--- src/renderer/rasterizer/rasterizer.h
+++ src/renderer/rasterizer/rasterizer.h
@@ -4,8 +4,8 @@
 
 #include <functional>
 #include <iostream>
-#include <linalg.h>
 #include <limits>
+#include <linalg.h>
 #include <memory>
 
 
@@ -19,13 +19,13 @@ namespace cg::renderer
 	class rasterizer
 	{
 	public:
-		rasterizer(){};
-		~rasterizer(){};
+		rasterizer() {};
+		~rasterizer() {};
 		void set_render_target(
 				std::shared_ptr<resource<RT>> in_render_target,
 				std::shared_ptr<resource<float>> in_depth_buffer = nullptr);
 		void clear_render_target(
-				const RT& in_clear_value, const float in_depth = DEFAULT_DEPTH);
+				const RT& in_clear_value, const float in_depth = 1);
 
 		void set_vertex_buffer(std::shared_ptr<resource<VB>> in_vertex_buffer);
 		void set_index_buffer(std::shared_ptr<resource<unsigned int>> in_index_buffer);
@@ -33,6 +33,8 @@ namespace cg::renderer
 		void set_viewport(size_t in_width, size_t in_height);
 
 		void draw(size_t num_vertexes, size_t vertex_offset);
+
+		void apply_sepia_tone_filter();
 
 		std::function<std::pair<float4, VB>(float4 vertex, VB vertex_data)> vertex_shader;
 		std::function<cg::color(const VB& vertex_data, const float z)> pixel_shader;
@@ -50,27 +52,66 @@ namespace cg::renderer
 		bool depth_test(float z, size_t x, size_t y);
 	};
 
+	template<typename VB, typename RT>// CREATIVE TASK
+	inline void rasterizer<VB, RT>::apply_sepia_tone_filter()
+	{
+		if (!render_target)
+			return;
+
+		for (size_t y = 0; y < height; ++y)
+		{
+			for (size_t x = 0; x < width; ++x)
+			{
+				auto& pixel = render_target->item(x, y);
+				// convert pixel to sepia
+				float tr = 0.393f * pixel.r + 0.769f * pixel.g + 0.189f * pixel.b;
+				float tg = 0.349f * pixel.r + 0.686f * pixel.g + 0.168f * pixel.b;
+				float tb = 0.272f * pixel.r + 0.534f * pixel.g + 0.131f * pixel.b;
+
+				// and clamp values to ensure valid range [0, 255]
+				pixel.r = static_cast<uint8_t>(std::min(255.0f, tr));
+				pixel.g = static_cast<uint8_t>(std::min(255.0f, tg));
+				pixel.b = static_cast<uint8_t>(std::min(255.0f, tb));
+			}
+		}
+	}
+
 	template<typename VB, typename RT>
 	inline void rasterizer<VB, RT>::set_render_target(
 			std::shared_ptr<resource<RT>> in_render_target,
 			std::shared_ptr<resource<float>> in_depth_buffer)
 	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
-		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
+		if (in_render_target)
+			render_target = in_render_target;
+		if (in_depth_buffer)
+			depth_buffer = in_depth_buffer;
 	}
 
 	template<typename VB, typename RT>
 	inline void rasterizer<VB, RT>::set_viewport(size_t in_width, size_t in_height)
 	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
+		height = in_height;
+		width = in_width;
 	}
 
 	template<typename VB, typename RT>
 	inline void rasterizer<VB, RT>::clear_render_target(
 			const RT& in_clear_value, const float in_depth)
 	{
-		// TODO Lab: 1.02 Implement `set_render_target`, `set_viewport`, `clear_render_target` methods of `cg::renderer::rasterizer` class
-		// TODO Lab: 1.06 Adjust `set_render_target`, and `clear_render_target` methods of `cg::renderer::rasterizer` class to consume a depth buffer
+		if (render_target)
+		{
+			for (size_t i = 0; i < render_target->count(); i++)
+			{
+				render_target->item(i) = in_clear_value;
+			}
+		}
+		if (depth_buffer)
+		{
+			for (size_t i = 0; i < depth_buffer->count(); i++)
+			{
+				depth_buffer->item(i) = in_depth;
+			}
+		}
 	}
 
 	template<typename VB, typename RT>
@@ -90,23 +131,89 @@ namespace cg::renderer
 	template<typename VB, typename RT>
 	inline void rasterizer<VB, RT>::draw(size_t num_vertexes, size_t vertex_offset)
 	{
-		// TODO Lab: 1.04 Implement `cg::world::camera` class
-		// TODO Lab: 1.05 Add `Rasterization` and `Pixel shader` stages to `draw` method of `cg::renderer::rasterizer`
-		// TODO Lab: 1.06 Add `Depth test` stage to `draw` method of `cg::renderer::rasterizer`
+		size_t vertex_id = vertex_offset;
+		while (vertex_id < vertex_offset + num_vertexes)
+		{
+			std::vector<VB> vertices(3);
+			vertices[0] = vertex_buffer->item(
+					index_buffer->item(vertex_id++));
+			vertices[1] = vertex_buffer->item(
+					index_buffer->item(vertex_id++));
+			vertices[2] = vertex_buffer->item(
+					index_buffer->item(vertex_id++));
+
+			for (auto& vertex: vertices)
+			{
+				float4 coords{vertex.x, vertex.y, vertex.z, 1.f};
+
+				auto processed_vertex = vertex_shader(coords, vertex);
+
+				vertex.x = processed_vertex.first.x / processed_vertex.first.w;
+				vertex.y = processed_vertex.first.y / processed_vertex.first.w;
+				vertex.z = processed_vertex.first.z / processed_vertex.first.w;
+
+				vertex.x = (vertex.x + 1.f) * width * .5f;
+				vertex.y = (-vertex.y + 1.f) * height * .5f;
+			}
+
+			int2 vertex_a = int2(static_cast<int>(vertices[0].x), static_cast<int>(vertices[0].y));
+			int2 vertex_b = int2(static_cast<int>(vertices[1].x), static_cast<int>(vertices[1].y));
+			int2 vertex_c = int2(static_cast<int>(vertices[2].x), static_cast<int>(vertices[2].y));
+
+			float edge = static_cast<float>(edge_function(vertex_a, vertex_b, vertex_c));
+
+			int2 min_border = int2{0, 0};
+			int2 max_border = int2{static_cast<int>(width - 1), static_cast<int>(height - 1)};
+
+			int2 min_vertex = min(vertex_a, min(vertex_b, vertex_c));
+			int2 bb_begin = clamp(min_vertex, min_border, max_border);
+
+			int2 max_vertex = max(vertex_a, max(vertex_b, vertex_c));
+			int2 bb_end = clamp(max_vertex, min_border, max_border);
+
+			for (int x = bb_begin.x; x <= bb_end.x; x++)
+			{
+				for (int y = bb_begin.y; y <= bb_end.y; y++)
+				{
+					int2 point(x, y);
+					int edge0 = edge_function(vertex_a, vertex_b, point);
+					int edge1 = edge_function(vertex_b, vertex_c, point);
+					int edge2 = edge_function(vertex_c, vertex_a, point);
+
+					if (edge0 >= 0 && edge1 >= 0 && edge2 >= 0)
+					{
+						float u = edge1 / edge;
+						float v = edge2 / edge;
+						float w = edge0 / edge;
+						float depth = u * vertices[0].z + v * vertices[1].z +
+									  w * vertices[2].z;
+
+						size_t u_x = static_cast<size_t>(x);
+						size_t u_y = static_cast<size_t>(y);
+
+						if (depth_test(depth, u_x, u_y))
+						{
+							auto pixel = pixel_shader(vertices[0], 0.f);
+							render_target->item(u_x, u_y) = RT::from_color(pixel);
+							if (depth_buffer)
+								depth_buffer->item(u_x, u_y) = depth;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	template<typename VB, typename RT>
 	inline int
 	rasterizer<VB, RT>::edge_function(int2 a, int2 b, int2 c)
 	{
-		// TODO Lab: 1.05 Implement `cg::renderer::rasterizer::edge_function` method
-		return 0;
+		return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
 	}
 
 	template<typename VB, typename RT>
 	inline bool rasterizer<VB, RT>::depth_test(float z, size_t x, size_t y)
 	{
-		// TODO Lab: 1.06 Implement `depth_test` function of `cg::renderer::rasterizer` class
 		if (!depth_buffer)
 		{
 			return true;
